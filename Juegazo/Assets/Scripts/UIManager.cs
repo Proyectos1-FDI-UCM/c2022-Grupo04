0bb268c5bd60293aed514f49709fa5a9079f9a98
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2022-03-08 17:58:23 +0100

    idk

--- Juegazo/Assets/Scripts/UIManager.cs
+++ Juegazo/Assets/Scripts/UIManager.cs
@@ -15,7 +15,46 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Sprite _imagentornillo;
     #endregion
+
+    #region references
+    [SerializeField]
+    public GameObject _myPanel;
+    [SerializeField]
+    private Canvas _myCanvas;
+    [SerializeField]
+    private Image _myImage;
+    #endregion
+
     #region methods
+    public void Pause()
+    {
+        _myPanel.SetActive(true);
+        GameManager.Instance.Pause();
+    }
+    public void Continue()
+    {
+        _myPanel.SetActive(false);
+        GameManager.Instance.Continue();
+    }
+    public void MainMenu()
+    {
+        _myPanel.SetActive(false);           //probably no hace falta pero idk
+        GameManager.Instance.MainMenu();
+    }
+    public void Quit()
+    {
+        _myPanel.SetActive(false);
+        GameManager.Instance.Quit();
+    }
+    public void StartGame()
+    {
+        GameManager.Instance.StartGame();
+    }
+    public void Controls()
+    {
+        _myPanel.SetActive(false);
+        _myImage.enabled = true;
+    }
     public void showTimer(float Timeleft)
     {
         _timeText.text = "Time: " + (int)Timeleft;
@@ -30,6 +69,8 @@ public class UIManager : MonoBehaviour
     {
         _timeText = ShowTime.GetComponent<Text>();
         _tornillosText = ShowTornillos.GetComponent<Text>();
+        _myImage.enabled = false;
+        _myPanel.SetActive(false);
     }
 
     // Update is called once per frame
