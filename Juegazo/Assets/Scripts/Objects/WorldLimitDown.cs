738ca768fc68aa0766453b074f5f01686629de15
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2022-04-05 23:59:52 +0200

    Cambios escena1 y WorldLimitDown Arreglado

--- Juegazo/Assets/Scripts/Objects/WorldLimitDown.cs
+++ Juegazo/Assets/Scripts/Objects/WorldLimitDown.cs
@@ -11,28 +11,23 @@ public class WorldLimitDown : MonoBehaviour
     private PlayerLifeComponent _playerLife;
     #endregion
 
-
     #region methods
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         EnemyThere _enemyThere;
         _enemyThere = collision.collider.GetComponent<EnemyThere>();
-        PlayerLifeComponent _playerLifeComponent = collision.collider.GetComponent<PlayerLifeComponent>();
+        PlayerLifeComponent _playerLifeComponent = collision.collider.GetComponentInParent<PlayerLifeComponent>();
         if (_enemyThere != null )
         {
             GameManager.Instance.m_boxesCount--;
             _playerRobotBox._listOfBoxes.Remove(collision.collider.gameObject);
             Destroy(collision.collider.gameObject);
         }
-
         if(_playerLifeComponent != null)
         {
             Debug.LogError("lol");
             _playerLife.Die();
         }
-
-
     }
     #endregion
     // Start is called before the first frame update
@@ -40,11 +35,5 @@ public class WorldLimitDown : MonoBehaviour
     {
         _playerRobotBox = _player.GetComponent<RobotBox>();
         _playerLife = _player.GetComponent<PlayerLifeComponent>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
